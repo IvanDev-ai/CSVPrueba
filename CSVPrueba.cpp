46616dc3627fd5346deba602b53fb1bd91aea422
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2024-12-17 14:52:13 +0100

    Prueba

---
+++ CSVPrueba.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "CSVPrueba.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::ifstream archivo("citas.csv");
+    if (!archivo) {
+        std::cout << "Error al abrir el archivo de citas." << std::endl;
+    }
+    std::string linea;
+    while (std::getline(archivo, linea)) {
+        std::cout << "Linea: " << linea << std::endl;
+    }
+
+
+    return 0;
+}
